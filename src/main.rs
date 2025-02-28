f9f462bc002aa1ba9f3f0d1d54fb839fe6db1192
Tom Webber <t.c.w.webber@gmail.com>
2025-02-28 18:44:35 +0100

    (main) add: actix started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::set_var;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+async fn index() -> impl Responder {
+    "Hello world!"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    unsafe { set_var("RUST_LOG", "debug") }
+    env_logger::init();
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .service(web::scope("/app").route("/index.html", web::get().to(index)))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
